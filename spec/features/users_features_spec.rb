0db09c4b0985fff1fed03ffc4aea679eeb983dd1
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-06-07 17:49:05 +0200

    User complete

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -19,6 +19,7 @@ feature "User can sign in and out" do
       visit('/')
       click_link('Sign up')
       fill_in('Email', with: 'test@example.com')
+      fill_in('User name', with: 'testuser')
       fill_in('Password', with: 'testtest')
       fill_in('Password confirmation', with: 'testtest')
       click_button('Sign up')
